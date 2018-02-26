d917a222ff33d44b251efd57338668f811c0e849
jaderyan <jadeelizabethryan@outlook.com>
2018-02-26 14:18:43 +0000

    check if current website is on database

---
+++ website.js
@@ -0,0 +1,8 @@
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    fetch(`https://cruelty-free-chrome-extension.herokuapp.com/api/companies?website=${tabs[0].url}`)
+      .then(res => 
+        res.status === 404 ? document.getElementById('result').textContent = 'Boo! This is not a cruelty free brand' : res.json())
+        .then(({company}) => {
+            document.getElementById('result').textContent = `Yay! ${company.name} are cruelty free. Treat yo self!`
+        })
+});
