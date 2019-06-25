38a64f52be8494251373d8d7fa3e4b902713440e
jaderyan <jadeelizabethryan@outlook.com>
2019-06-25 20:38:53 +0100

    add loading message

--- resources/js/brand.js
+++ resources/js/brand.js
@@ -1,13 +1,25 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-    document.getElementById('search').addEventListener('click', clickHandler, false);
-  });
+document.addEventListener("DOMContentLoaded", function(event) {
+  document
+    .getElementById("search")
+    .addEventListener("click", clickHandler, false);
+});
 
 function clickHandler(event) {
-    event.preventDefault()
-    const company = document.getElementById('company').value;
-    fetch(`https://cruelty-free-chrome-extension.herokuapp.com/api/companies/${company}`)
-      .then(res => res.status === 404 ? document.getElementById('brand-result').textContent = 'Boo! This is not a cruelty free brand.' : res.json())
-      .then(({company}) => {
-        document.getElementById('brand-result').textContent = `Yay! ${company.name} are cruelty free. Treat yo self!`
-      })
+  event.preventDefault();
+  const company = document.getElementById("company").value;
+  document.getElementById("brand-result").textContent = `Searching ...`;
+  fetch(
+    `https://cruelty-free-chrome-extension.herokuapp.com/api/companies/${company}`
+  )
+    .then(res =>
+      res.status === 404
+        ? (document.getElementById("brand-result").textContent =
+            "Boo! This is not a cruelty free brand.")
+        : res.json()
+    )
+    .then(({ company }) => {
+      document.getElementById("brand-result").textContent = `Yay! ${
+        company.name
+      } are cruelty free. Treat yo self!`;
+    });
 }
